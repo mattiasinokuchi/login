ea217ca0f59dee11d1bea0af4a3643098f07ec8b
Mattias Inokuchi <46524414+mattiasinokuchi@users.noreply.github.com>
2020-10-12 13:58:20 +0200

    Update dev.js

--- config/dev.js
+++ config/dev.js
@@ -1,4 +1,1 @@
-module.exports = {
-  MONGO_URI:  "mongodb+srv://user:<password>@cluster0.5u6gt.mongodb.net/<dbname>?retryWrites=true&w=majority",  // add mongoDB URI here
-  SECRET: "pappa"      ,// add Secret here
-};
+
